2fb772dc0f1c6975f344ba170e6f269e36d6c8ee
Lâm Bảo Duy <thirdteenth.octt@gmail.com>
2023-11-18 01:04:11 +0700

    category

--- shopapp/src/main/java/com/sales/shopapp/controller/CategoryController.java
+++ shopapp/src/main/java/com/sales/shopapp/controller/CategoryController.java
@@ -1,20 +1,39 @@
 package com.sales.shopapp.controller;
 
+import com.sales.shopapp.dto.CategoryDto;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/categories")
-
+//@Validated
 public class CategoryController {
     @GetMapping("")
-    public ResponseEntity<String> getAllCategories() {
-        return ResponseEntity.ok("oke");
+    public ResponseEntity<String> getAllCategories(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit
+    ) {
+        return ResponseEntity.ok(String.valueOf(page + limit));
     }
 
     @PostMapping("")
-    public ResponseEntity<String> insertCategory(){
-        return ResponseEntity.ok("post oke");
+    public ResponseEntity<?> insertCategory(
+            @Valid @RequestBody CategoryDto categoryDto,
+            BindingResult result){
+        if (result.hasErrors()){
+            List<String> errorMessage = result.getFieldErrors()
+                    .stream()
+                    .map(fieldError -> fieldError.getDefaultMessage())
+                    .toList();
+            return ResponseEntity.badRequest().body(errorMessage);
+        }
+        return ResponseEntity.ok(categoryDto.toString());
     }
 
     @PutMapping("/{id}")
---
+++ shopapp/src/main/java/com/sales/shopapp/dto/CategoryDto.java
@@ -0,0 +1,14 @@
+package com.sales.shopapp.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+    @NotEmpty(message = "Name can not null")
+    private String name ;
+}
